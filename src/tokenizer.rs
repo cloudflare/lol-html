247c56497f782ec02247fde635c2383787a1a464
Ivan Nikulin <inikulin@cloudflare.com>
2018-07-13 21:43:26 +0100

    Add test data

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,19 +1,27 @@
-pub struct BufferCapacityExceededError<'c> {
-    unprocessed_chunk: &'c [u8],
+pub enum Token {
+    Eof,
 }
 
-pub struct Tokenizer {
+pub struct BufferCapacityExceededError<'c> {
+    unprocessed_buffer: &'c [u8],
+}
+
+pub struct Tokenizer<'t> {
     buffer: Box<[u8]>,
     buffer_capacity: usize,
     buffer_watermark: usize,
+    token_handler: &'t FnMut(&Token),
+    state: fn(&mut Tokenizer<'t>),
 }
 
-impl Tokenizer {
-    fn new(buffer_capacity: usize) -> Tokenizer {
+impl<'t> Tokenizer<'t> {
+    fn new(token_handler: &FnMut(&Token), buffer_capacity: usize) -> Tokenizer {
         Tokenizer {
             buffer: Vec::with_capacity(buffer_capacity).into_boxed_slice(),
             buffer_capacity,
             buffer_watermark: 0,
+            token_handler,
+            state: Tokenizer::data_state,
         }
     }
 
@@ -22,10 +30,17 @@ impl Tokenizer {
 
         if self.buffer_watermark + chunk_len > self.buffer_capacity {
             return Err(BufferCapacityExceededError {
-                unprocessed_chunk: &self.buffer[0..self.buffer_watermark],
+                unprocessed_buffer: &self.buffer[0..self.buffer_watermark],
             });
         }
 
+        let new_watermark = self.buffer_watermark + chunk_len;
+
+        (&mut self.buffer[self.buffer_watermark..new_watermark]).copy_from_slice(&chunk);
+        self.buffer_watermark = new_watermark;
+
         Ok(())
     }
+
+    fn data_state(&mut self) {}
 }
