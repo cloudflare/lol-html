c23c63ebfccb75c83227402c90368f688d353d0a
Ivan Nikulin <inikulin@cloudflare.com>
2018-07-31 16:29:08 +0100

    State machine DSL: refactor state macro into separate file

--- src/tokenizer/state_machine_dsl/mod.rs
+++ src/tokenizer/state_machine_dsl/mod.rs
@@ -7,44 +7,16 @@ mod action_list;
 #[macro_use]
 mod state_body;
 
-macro_rules! state_group {
-    ( $($states:tt)+ ) => {
-        impl<'t, H: FnMut(LexResult)> Tokenizer<'t, H> {
-           state!($($states)+);
-        }
-    };
-}
-
-macro_rules! state {
-    // NOTE: wrap optional visibility modifier in `[]` to avoid
-    // local ambiguity with the state name.
-    ( pub $($rest:tt)* ) => ( state!([pub] $($rest)*); );
-
-    (
-        $([ $vis:ident ])* $name:ident $(<-- ( $($enter_actions:tt)* ))* {
-            $($arms:tt)*
-        }
-
-        $($rest:tt)*
-    ) => {
-        $($vis)* fn $name(&mut self, ch: Option<u8>) {
-            action_list!(@state_enter |self|> $($($enter_actions)*)*);
-            state_body!(| [self, ch] |> $($arms)*);
-        }
-
-        state!($($rest)*);
-    };
-
-
-    // NOTE: end of the state list
-    () => ();
-}
+#[macro_use]
+mod state;
 
 macro_rules! define_state_group {
     ( $name:ident = { $($states:tt)+ } ) => {
         macro_rules! $name {
             () => {
-                state_group!($($states)*);
+                impl<'t, H: FnMut(LexResult)> Tokenizer<'t, H> {
+                    state!($($states)+);
+                }
             };
         }
     };
---
+++ src/tokenizer/state_machine_dsl/state.rs
@@ -0,0 +1,24 @@
+macro_rules! state {
+    // NOTE: wrap optional visibility modifier in `[]` to avoid
+    // local ambiguity with the state name.
+    ( pub $($rest:tt)* ) => ( state!([pub] $($rest)*); );
+
+    (
+        $([ $vis:ident ])* $name:ident $(<-- ( $($enter_actions:tt)* ))* {
+            $($arms:tt)*
+        }
+
+        $($rest:tt)*
+    ) => {
+        $($vis)* fn $name(&mut self, ch: Option<u8>) {
+            action_list!(@state_enter |self|> $($($enter_actions)*)*);
+            state_body!(| [self, ch] |> $($arms)*);
+        }
+
+        state!($($rest)*);
+    };
+
+
+    // NOTE: end of the state list
+    () => ();
+}
