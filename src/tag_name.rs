50eb3ef4ff1a708d65503ce86c113ebe9138c14e
Ivan Nikulin <inikulin@cloudflare.com>
2018-10-04 18:26:59 +0100

    Change comment format for the tag name hash

--- src/tag_name.rs
+++ src/tag_name.rs
@@ -1,3 +1,27 @@
+//! All standard tag names contain only ASCII alpha characters
+//! and digits from 1 to 6 (in numbered header tags, i.e. <h1> - <h6>).
+//! Considering that tag names are case insensitive we have only
+//! 26 + 6 = 32 characters. Thus, single character can be encoded in
+//! 5 bits and we can fit up to 64 / 5 ≈ 12 characters in a 64-bit
+//! integer. This is enough to encode all standard tag names, so
+//! we can just compare integers instead of expensive string
+//! comparison for tag names.
+//!
+//! The original idea of this tag hash-like thing belongs to Ingvar
+//! Stepanyan and was implemented in lazyhtml. So, kudos to him for
+//! comming up with this cool optimisation. This implementation differs
+//! from the original one as it adds ability to encode digits from 1
+//! to 6 which allows us to encode numbered header tags.
+//!
+//! In this implementation we reserve numbers from 0 to 5 for digits
+//! from 1 to 6 and numbers from 6 to 31 for ASCII alphas. Otherwise,
+//! if we use numbers from 0 to 25 for ASCII alphas we'll have an
+//! ambiguity for repetitative `a` characters: both `a`,
+//! `aaa` and even `aaaaa` will give us 0 as a hash. It's still a case
+//! for digits, but considering that tag name can't start from digit
+//! we are safe here, since we'll just get first character shifted left
+//! by zeroes as repetitave 1 digits get added to the hash.
+
 // NOTE: unfortunately `static fn` is still unavaliable in stable,
 // so we need to use manually precalculated values in this enum.
 // Consistency between hashing algorithm and these values is guaranteed
@@ -89,29 +113,6 @@ impl PartialEq<TagName> for u64 {
 }
 
 impl TagName {
-    // NOTE: All standard tag names contain only ASCII alpha characters
-    // and digits from 1 to 6 (in numbered header tags, i.e. <h1> - <h6>).
-    // Considering that tag names are case insensitive we have only
-    // 26 + 6 = 32 characters. Thus, single character can be encoded in
-    // 5 bits and we can fit up to 64 / 5 ≈ 12 characters in a 64-bit
-    // integer. This is enough to encode all standard tag names, so
-    // we can just compare integers instead of expensive string
-    // comparison for tag names.
-    //
-    // The original idea of this tag hash-like thing belongs to Ingvar
-    // Stepanyan and was implemented in lazyhtml. So, kudos to him for
-    // comming up with this cool optimisation. This implementation differs
-    // from the original one as it adds ability to encode digits from 1
-    // to 6 which allows us to encode numbered header tags.
-    //
-    // In this implementation we reserve numbers from 0 to 5 for digits
-    // from 1 to 6 and numbers from 6 to 31 for ASCII alphas. Otherwise,
-    // if we use numbers from 0 to 25 for ASCII alphas we'll have an
-    // ambiguity for repetitative `a` characters: both `a`,
-    // `aaa` and even `aaaaa` will give us 0 as a hash. It's still a case
-    // for digits, but considering that tag name can't start from digit
-    // we are safe here, since we'll just get first character shifted left
-    // by zeroes as repetitave 1 digits get added to the hash.
     #[inline]
     pub fn update_hash(hash: Option<u64>, ch: u8) -> Option<u64> {
         let mut hash = hash;
