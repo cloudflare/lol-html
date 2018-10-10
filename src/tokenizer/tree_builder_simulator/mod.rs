844dc6b8bcc8bb8bafb39a9e8883789e5633f760
Ivan Nikulin <inikulin@cloudflare.com>
2018-10-10 14:28:17 +0100

    Add comment describing TreeBuilderSimulator

--- src/tokenizer/tree_builder_simulator/mod.rs
+++ src/tokenizer/tree_builder_simulator/mod.rs
@@ -1,3 +1,14 @@
+//! HTML tokenizer has 6 different state machines for text parsing
+//! purposes in different contexts. Switch between these state machines
+//! usually performed by the tree construction stage depending on the
+//! state of the stack of open elements (HTML is a context-sensitive grammar).
+//! Luckily, in the majority of cases this tree construction stage feedback
+//! can be simulated without the stack of open elements and comlicated rules
+//! required to maintain its state. This module implements such feedback
+//! simulation. However, there are few cases where we can't unambiguously
+//! determine parsing context and prefer to bail out from the tokenization
+//!  in such a case (see `TextParsingAmbiguityTracker` for the details).
+
 mod text_parsing_ambiguity;
 mod text_parsing_mode;
 
