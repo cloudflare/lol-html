3d4849d80a3595eac6a752ec9a20ea290f034040
Ivan Nikulin <inikulin@cloudflare.com>
2018-10-29 22:59:17 +0000

    Update todo list

--- src/lib.rs
+++ src/lib.rs
@@ -11,17 +11,13 @@ pub mod transform_stream;
 
 // TODO
 // -- Functionality
+// 1. Extract methods for actions
 // 2. TokenizerQuery
 // 3. Eager tokenizer
 // 4. Tokenizer driver
 // 5. Adjustable limits
-// 6. Get rid of token view as we don't need to store buffer anymore
 //
 // -- Performance
-// 1. Implement benchmark
-// 2. Get rid of dynamic dispatch for input (chunk from buffer)
-// 3. LTO
-// 4. In-state loops
 // 5. Don't emit character immidiately, extend existing
 // 6. State embedding
 
