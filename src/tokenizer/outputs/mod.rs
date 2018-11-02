a111ee957814ca5e6d9369bb5342063dc0ae1ff1
Ivan Nikulin <inikulin@cloudflare.com>
2018-11-02 13:18:14 +0000

    Implement TagPreview

--- src/tokenizer/outputs/mod.rs
+++ src/tokenizer/outputs/mod.rs
@@ -1,7 +1,9 @@
 mod lex_unit;
+mod tag_preview;
 mod token;
 mod token_view;
 
 pub use self::lex_unit::*;
+pub use self::tag_preview::*;
 pub use self::token::*;
 pub use self::token_view::*;
---
+++ src/tokenizer/outputs/tag_preview.rs
@@ -0,0 +1,29 @@
+use base::{Bytes, Chunk, Range};
+use lazycell::LazyCell;
+
+pub struct TagNameInfo<'c> {
+    input: &'c Chunk<'c>,
+    name_range: Range,
+    name: LazyCell<Bytes<'c>>,
+    pub name_hash: Option<u64>,
+}
+
+impl<'c> TagNameInfo<'c> {
+    fn new(input: &'c Chunk<'c>, name_range: Range, name_hash: Option<u64>) -> Self {
+        TagNameInfo {
+            input,
+            name_range,
+            name: LazyCell::new(),
+            name_hash,
+        }
+    }
+
+    pub fn get_name(&self) -> &Bytes<'c> {
+        self.name.borrow_with(|| self.input.slice(self.name_range))
+    }
+}
+
+pub enum TagPreview<'c> {
+    StartTag(TagNameInfo<'c>),
+    EndTag(TagNameInfo<'c>),
+}
