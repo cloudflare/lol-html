f1919ece603158fbcf1a4dbce8e2760819c58e80
Ivan Nikulin <inikulin@cloudflare.com>
2019-01-19 22:50:51 +0000

    Implement Serialize and mutation API for EndTag

--- src/token/impls/tags/end_tag.rs
+++ src/token/impls/tags/end_tag.rs
@@ -1,9 +1,10 @@
 use super::try_tag_name_from_str;
 use crate::base::Bytes;
+use crate::transform_stream::Serialize;
 use encoding_rs::Encoding;
 use failure::Error;
+use std::fmt::{self, Debug};
 
-#[derive(Debug)]
 pub struct EndTag<'i> {
     name: Bytes<'i>,
     raw: Option<Bytes<'i>>,
@@ -24,4 +25,38 @@ impl<'i> EndTag<'i> {
     }
 
     implement_tag_name_accessors!();
+
+    // NOTE: not a trait implementation due to the `Borrow` constraint for
+    // the `Owned` associated type.
+    // See: https://github.com/rust-lang/rust/issues/44950
+    #[inline]
+    pub fn to_owned(&self) -> EndTag<'static> {
+        EndTag {
+            name: self.name.to_owned(),
+            raw: self.raw.as_ref().map(|r| r.to_owned()),
+            encoding: self.encoding,
+        }
+    }
+}
+
+impl Serialize for EndTag<'_> {
+    #[inline]
+    fn take_raw(&mut self) -> Option<Bytes<'_>> {
+        self.raw.take()
+    }
+
+    #[inline]
+    fn serialize_from_parts(self, handler: &mut dyn FnMut(Bytes<'_>)) {
+        handler(b"</".into());
+        handler(self.name);
+        handler(b">".into());
+    }
+}
+
+impl Debug for EndTag<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("EndTag")
+            .field("name", &self.name())
+            .finish()
+    }
 }
