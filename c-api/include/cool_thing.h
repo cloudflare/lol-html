3382c10bd791eed01bfe8c7413beeacc633754c7
Ivan Nikulin <inikulin@cloudflare.com>
2019-03-08 14:00:21 +0000

    Start implementing C-API

---
+++ c-api/include/cool_thing.h
@@ -0,0 +1,17 @@
+#ifndef COOL_THING_H
+#define COOL_THING_H
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+typedef DocumentContentHandlers cool_thing_document_content_handlers;
+
+cool_thing_document_content_handlers *cool_thing_new_document_content_handlers();
+
+
+#if defined(__cplusplus)
+}  // extern C
+#endif
+
+#endif // COOL_THING_H
---
+++ c-api/src/lib.rs
@@ -0,0 +1,6 @@
+use cool_thing::DocumentContentHandlers;
+
+#[no_mangle]
+pub extern fn cool_thing_new_document_content_handlers() -> *mut DocumentContentHandlers<'static> {
+    Box::into_raw(Box::new(DocumentContentHandlers::default()))
+}
