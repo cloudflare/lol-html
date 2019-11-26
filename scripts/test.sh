38bab3c78037c4ebe37427c788f94ad5c1798be1
Gabriela Alexandra Moldovan <gabi@cloudflare.com>
2019-11-26 17:28:31 +0000

    Test that all the tools compile.

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,5 +9,9 @@ cargo test --features=integration_test "$@"
 echo "=== Running C API tests... ==="
 prove -e 'cargo' run ::  --manifest-path=./c-api/tests/Cargo.toml
 
-echo "=== Building fuzzing test case code to ensure that it uses recent API... ==="
+echo "=== Building fuzzing test case code to ensure that it uses current API... ==="
 (cd fuzz/test_case && cargo build)
+
+echo "=== Building the tooling test case code to ensure it uses the current API... ==="
+(cd tools/parser_trace/ && cargo build)
+(cd tools/selectors_ast/ && cargo build)
