fdc21ea565c39175a553747d2bffe02bac57545a
John Bampton <jbampton@users.noreply.github.com>
2020-07-01 22:22:22 +1000

    Use quotes to prevent word splitting.
    Checked with ShellCheck.
    https://www.shellcheck.net/

--- scripts/precommit.sh
+++ scripts/precommit.sh
@@ -6,4 +6,4 @@ set -e
 (cd c-api && cargo fmt --all)
 (cd c-api/tests && cargo fmt --all)
 (cd fuzz && cargo fmt --all)
-cargo fmt --all && git add $(git status --porcelain=v2 | awk 'BEGIN {ORS=" "}; $2 == "MM" {print $9}')
+cargo fmt --all && git add "$(git status --porcelain=v2 | awk 'BEGIN {ORS=" "}; $2 == "MM" {print $9}')"
