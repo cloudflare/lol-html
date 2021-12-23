7a9b34266250c5ed55e34cced6db7f8200ed5fb3
Joshua Nelson <jnelson@cloudflare.com>
2021-12-23 16:34:24 -0600

    Fix libfuzzer script
    - Build C API before running the fuzzer
    - Add liblolhtml.so to linker path
    - Fix compile error in test case
    - Install `cargo-fuzz` if it's not already installed

--- scripts/fuzz_with_libfuzzer.sh
+++ scripts/fuzz_with_libfuzzer.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
-set -e
+set -eu
 
-cd ./fuzz/ && \
-cargo +nightly build && \
-cd .. & \
+cargo +nightly install cargo-fuzz
+(
+	cd ./c-api
+	# ok to use default toolchain here since C ABI is stable
+	cargo build
+)
+# add `liblolhtml.so` to linker path
+export LD_LIBRARY_PATH="$(realpath ${CARGO_TARGET_DIR:-c-api/target}/debug/deps)"
+(
+	cd ./fuzz
+	cargo +nightly build
+)
 rustup run nightly cargo-fuzz run --jobs 24 --release fuzz_rewriter fuzz/corpus/selector_matching
