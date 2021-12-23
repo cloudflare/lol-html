35e892757ebec597ca7496bdc0dea2e0c7a3a77b
Joshua Nelson <jnelson@cloudflare.com>
2021-12-23 16:43:32 -0600

    Make `scripts/test.sh` faster to run
    Since it's only checking whether the tests build at all, it can use `check` rather than `build`.

--- scripts/test.sh
+++ scripts/test.sh
@@ -10,8 +10,8 @@ echo "=== Running C API tests... ==="
 prove -e 'cargo' run ::  --manifest-path=./c-api/tests/Cargo.toml
 
 echo "=== Building fuzzing test case code to ensure that it uses current API... ==="
-(cd fuzz/test_case && cargo build)
+(cd fuzz/test_case && cargo check)
 
 echo "=== Building the tooling test case code to ensure it uses the current API... ==="
-(cd tools/parser_trace/ && cargo build)
-(cd tools/selectors_ast/ && cargo build)
+(cd tools/parser_trace/ && cargo check)
+(cd tools/selectors_ast/ && cargo check)
