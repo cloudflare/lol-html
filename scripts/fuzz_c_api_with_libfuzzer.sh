9c541c7042dbd46c559569f48ff3a1a50f221e29
Joshua Nelson <jnelson@cloudflare.com>
2021-12-23 17:00:14 -0600

    Fix fuzz_c_api_with_libfuzzer script

--- scripts/fuzz_c_api_with_libfuzzer.sh
+++ scripts/fuzz_c_api_with_libfuzzer.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-cd ./c-api && cargo +nightly build && \
-cd ../fuzz && cargo +nightly build && \
-cd ..  && \
+cd ./c-api && cargo +nightly build
+cd ../fuzz && cargo +nightly build
+cd ..
+# add `liblolhtml.so` to linker path
+export LD_LIBRARY_PATH="$(realpath ${CARGO_TARGET_DIR:-c-api/target}/debug/deps)"
 rustup run nightly cargo-fuzz run --jobs 24 --release fuzz_c_api fuzz/corpus/selector_matching
