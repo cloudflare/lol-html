a47a126b9ce2be3e5b7bd9890f535140512f1d75
クリボーン <87871320+Kuribohrn@users.noreply.github.com>
2023-04-22 10:17:59 -0400

    fix(fuzz): fix honggfuzz fuzzing script
    This commit fixes the broken `honggfuzz` fuzzing script by adding  `lolhtml` library at the linking stage and runtime.

--- scripts/fuzz_with_hongg.sh
+++ scripts/fuzz_with_hongg.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 
-set -e
+set -eu
 
-cd ./fuzz/hongg && \
-cargo +nightly build && \
-RUSTFLAGS="-Z sanitizer=address" HFUZZ_RUN_ARGS="--threads=4" HFUZZ_INPUT=../corpus/selector_matching cargo +nightly hfuzz run hongg
+# In case you're missing libraries, do:
+# sudo apt-get install binutils-dev libunwind-dev
+cargo +nightly install honggfuzz
+(
+	cd ./c-api
+	# ok to use default toolchain here since C ABI is stable
+	cargo build
+)
+LOLHTML_LIB_PATH="$(realpath ${CARGO_TARGET_DIR:-c-api/target}/debug/deps)"
+# add `liblolhtml.so` to compiling linker path
+export RUSTFLAGS="-L $LOLHTML_LIB_PATH"
+# add `liblolhtml.so` to runtime linker path
+export LD_LIBRARY_PATH="$LOLHTML_LIB_PATH"
+(
+	cd ./fuzz/hongg
+	cargo +nightly build
+    HFUZZ_INPUT=../corpus/selector_matching cargo +nightly hfuzz run hongg
+)
