38ebfacf1f1c91959f79f36a7f3da46121762ae8
クリボーン <87871320+Kuribohrn@users.noreply.github.com>
2023-04-22 10:00:08 -0400

    fix(fuzz): fix afl fuzzing script
    This commit fixes the broken `afl` fuzzing script by adding  `lolhtml` library at the linking stage and runtime.

--- scripts/fuzz_with_afl.sh
+++ scripts/fuzz_with_afl.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
-set -e
+set -eu
 
-cd ./fuzz/afl && \
-cargo +nightly afl build && \
-RUSTFLAGS="-Z sanitizer=address" cargo +nightly afl fuzz -x ../dictionaries -i ../corpus/selector_matching -o out target/debug/afl-fuzz
+cargo +nightly install afl
+(
+	cd ./c-api
+	# ok to use default toolchain here since C ABI is stable
+	cargo build
+)
+LOLHTML_LIB_PATH="$(realpath ${CARGO_TARGET_DIR:-c-api/target}/debug/deps)"
+# add `liblolhtml.so` to compiling linker path
+export RUSTFLAGS="-L $LOLHTML_LIB_PATH"
+# add `liblolhtml.so` to runtime linker path
+export LD_LIBRARY_PATH="$LOLHTML_LIB_PATH"
+(
+	cd ./fuzz/afl
+	cargo +nightly afl build
+    cargo +nightly afl fuzz -x ../dictionaries -i ../corpus/selector_matching -o out target/debug/afl-fuzz
+)
