0757e6b4dd68e0640dd730f20e1d35d312a50d16
Kornel <kornel@cloudflare.com>
2024-11-01 00:23:05 +0000

    Functions dereferencing arbitrary pointers must be unsafe

--- c-api/src/doctype.rs
+++ c-api/src/doctype.rs
@@ -1,27 +1,30 @@
 use super::*;
 
 #[no_mangle]
-pub extern "C" fn lol_html_doctype_name_get(doctype: *const Doctype) -> Str {
+pub unsafe extern "C" fn lol_html_doctype_name_get(doctype: *const Doctype) -> Str {
     Str::from_opt(to_ref!(doctype).name())
 }
 
 #[no_mangle]
-pub extern "C" fn lol_html_doctype_public_id_get(doctype: *const Doctype) -> Str {
+pub unsafe extern "C" fn lol_html_doctype_public_id_get(doctype: *const Doctype) -> Str {
     Str::from_opt(to_ref!(doctype).public_id())
 }
 
 #[no_mangle]
-pub extern "C" fn lol_html_doctype_system_id_get(doctype: *const Doctype) -> Str {
+pub unsafe extern "C" fn lol_html_doctype_system_id_get(doctype: *const Doctype) -> Str {
     Str::from_opt(to_ref!(doctype).system_id())
 }
 
 #[no_mangle]
-pub extern "C" fn lol_html_doctype_user_data_set(doctype: *mut Doctype, user_data: *mut c_void) {
+pub unsafe extern "C" fn lol_html_doctype_user_data_set(
+    doctype: *mut Doctype,
+    user_data: *mut c_void,
+) {
     to_ref_mut!(doctype).set_user_data(user_data);
 }
 
 #[no_mangle]
-pub extern "C" fn lol_html_doctype_user_data_get(doctype: *const Doctype) -> *mut c_void {
+pub unsafe extern "C" fn lol_html_doctype_user_data_get(doctype: *const Doctype) -> *mut c_void {
     get_user_data!(doctype)
 }
 
