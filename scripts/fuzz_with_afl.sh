d7e738ed30503a04ca05abf8dd2f1d5b68fe0262
Kornel <kornel@cloudflare.com>
2024-11-08 15:14:55 +0000

    Auto-install fuzzers

--- scripts/fuzz_with_afl.sh
+++ scripts/fuzz_with_afl.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+cargo +nightly install cargo-afl
+
 cd ./fuzz/afl && \
 cargo +nightly afl build && \
 RUSTFLAGS="-Z sanitizer=address" cargo +nightly afl fuzz -x ../dictionaries -i ../corpus/selector_matching -o out target/debug/afl-fuzz
