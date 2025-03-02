e95998e76be8525c0fd9aff8b22388127f97bdf2
Tai NA <taina0407@users.noreply.github.com>
2025-03-02 21:17:52 +0700

    Add EndTag.removed property to consolidate impl_mutations_end_tag

--- js-api/src/end_tag.rs
+++ js-api/src/end_tag.rs
@@ -5,7 +5,7 @@ use lol_html::html_content::EndTag as NativeEndTag;
 pub struct EndTag(NativeRefWrap<NativeEndTag<'static>>);
 
 impl_from_native!(NativeEndTag => EndTag);
-impl_mutations_end_tag!(EndTag);
+impl_mutations!(EndTag);
 
 #[wasm_bindgen]
 impl EndTag {
