fe22da6af563b8c06ff4e7079d2b0a657a671c90
Kornel <kornel@cloudflare.com>
2026-02-20 12:00:13 +0000

    DRY

--- c-api/src/rewriter.rs
+++ c-api/src/rewriter.rs
@@ -34,6 +34,40 @@ impl OutputSink for ExternOutputSink {
     }
 }
 
+fn lol_html_rewriter_build_inner(
+    builder: *mut HtmlRewriterBuilder,
+    encoding: *const c_char,
+    encoding_len: size_t,
+    memory_settings: MemorySettings,
+    output_sink: unsafe extern "C" fn(*const c_char, size_t, *mut c_void),
+    output_sink_user_data: *mut c_void,
+    strict: bool,
+    enable_esi_tags: bool,
+) -> Result<HtmlRewriter, Box<dyn std::error::Error>> {
+    let builder = to_ref!(builder);
+    let handlers = builder.get_safe_handlers();
+
+    let maybe_encoding =
+        encoding_rs::Encoding::for_label_no_replacement(to_bytes!(encoding, encoding_len));
+    let encoding = maybe_encoding.ok_or(EncodingError::UnknownEncoding)?;
+    let settings = Settings {
+        element_content_handlers: handlers.element,
+        document_content_handlers: handlers.document,
+        encoding: encoding
+            .try_into()
+            .or(Err(EncodingError::NonAsciiCompatibleEncoding))?,
+        memory_settings,
+        strict,
+        enable_esi_tags,
+        adjust_charset_on_meta_tag: false,
+    };
+
+    let output_sink = ExternOutputSink::new(output_sink, output_sink_user_data);
+    let rewriter = lol_html::HtmlRewriter::new(settings, output_sink);
+
+    Ok(HtmlRewriter(Some(rewriter)))
+}
+
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn lol_html_rewriter_build(
     builder: *mut HtmlRewriterBuilder,
@@ -44,26 +78,9 @@ pub unsafe extern "C" fn lol_html_rewriter_build(
     output_sink_user_data: *mut c_void,
     strict: bool,
 ) -> *mut HtmlRewriter {
-    let builder = to_ref!(builder);
-    let handlers = builder.get_safe_handlers();
-
-    let maybe_encoding =
-        encoding_rs::Encoding::for_label_no_replacement(to_bytes!(encoding, encoding_len));
-    let encoding = unwrap_or_ret_null! { maybe_encoding.ok_or(EncodingError::UnknownEncoding) };
-    let settings = Settings {
-        element_content_handlers: handlers.element,
-        document_content_handlers: handlers.document,
-        encoding: unwrap_or_ret_null! { encoding.try_into().or(Err(EncodingError::NonAsciiCompatibleEncoding)) },
-        memory_settings,
-        strict,
-        enable_esi_tags: false,
-        adjust_charset_on_meta_tag: false,
-    };
-
-    let output_sink = ExternOutputSink::new(output_sink, output_sink_user_data);
-    let rewriter = lol_html::HtmlRewriter::new(settings, output_sink);
-
-    to_ptr_mut(HtmlRewriter(Some(rewriter)))
+    to_ptr_mut(unwrap_or_ret_null! {
+        lol_html_rewriter_build_inner(builder, encoding, encoding_len, memory_settings, output_sink, output_sink_user_data, strict, false)
+    })
 }
 
 #[unsafe(no_mangle)]
@@ -76,26 +93,9 @@ pub unsafe extern "C" fn unstable_lol_html_rewriter_build_with_esi_tags(
     output_sink_user_data: *mut c_void,
     strict: bool,
 ) -> *mut HtmlRewriter {
-    let builder = to_ref!(builder);
-    let handlers = builder.get_safe_handlers();
-
-    let maybe_encoding =
-        encoding_rs::Encoding::for_label_no_replacement(to_bytes!(encoding, encoding_len));
-    let encoding = unwrap_or_ret_null! { maybe_encoding.ok_or(EncodingError::UnknownEncoding) };
-    let settings = Settings {
-        element_content_handlers: handlers.element,
-        document_content_handlers: handlers.document,
-        encoding: unwrap_or_ret_null! { encoding.try_into().or(Err(EncodingError::NonAsciiCompatibleEncoding)) },
-        memory_settings,
-        strict,
-        enable_esi_tags: true,
-        adjust_charset_on_meta_tag: false,
-    };
-
-    let output_sink = ExternOutputSink::new(output_sink, output_sink_user_data);
-    let rewriter = lol_html::HtmlRewriter::new(settings, output_sink);
-
-    to_ptr_mut(HtmlRewriter(Some(rewriter)))
+    to_ptr_mut(unwrap_or_ret_null! {
+        lol_html_rewriter_build_inner(builder, encoding, encoding_len, memory_settings, output_sink, output_sink_user_data, strict, true)
+    })
 }
 
 #[unsafe(no_mangle)]
