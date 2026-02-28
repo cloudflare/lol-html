5ff27957cde035305c79d9d695cb229904aabe99
Kornel <kornel@cloudflare.com>
2026-02-28 00:14:16 +0000

    Don't reallocate strings when compiling

--- src/selectors_vm/compiler.rs
+++ src/selectors_vm/compiler.rs
@@ -33,11 +33,11 @@ pub(crate) struct AttrExprOperands {
 
 impl Expr<OnTagNameExpr> {
     #[inline]
-    pub fn compile_expr<F: Fn(&SelectorState<'_>, &LocalName<'_>) -> bool + Send + 'static>(
-        &self,
+    fn compile_expr<F: Fn(&SelectorState<'_>, &LocalName<'_>) -> bool + Send + 'static>(
+        negation: bool,
         f: F,
     ) -> CompiledLocalNameExpr {
-        if self.negation {
+        if negation {
             Box::new(move |s, a| !f(s, a))
         } else {
             Box::new(f)
@@ -47,7 +47,7 @@ impl Expr<OnTagNameExpr> {
 
 trait Compilable {
     fn compile(
-        &self,
+        self,
         encoding: &'static Encoding,
         exprs: &mut ExprSet,
         enable_nth_of_type: &mut bool,
@@ -56,30 +56,33 @@ trait Compilable {
 
 impl Compilable for Expr<OnTagNameExpr> {
     fn compile(
-        &self,
+        self,
         encoding: &'static Encoding,
         exprs: &mut ExprSet,
         enable_nth_of_type: &mut bool,
     ) {
-        let expr = match &self.simple_expr {
-            OnTagNameExpr::ExplicitAny => self.compile_expr(|_, _| true),
-            OnTagNameExpr::Unmatchable => self.compile_expr(|_, _| false),
+        let neg = self.negation;
+        let expr = match self.simple_expr {
+            OnTagNameExpr::ExplicitAny => Self::compile_expr(neg, |_, _| true),
+            OnTagNameExpr::Unmatchable => Self::compile_expr(neg, |_, _| false),
             OnTagNameExpr::LocalName(local_name) => {
-                match LocalName::from_str_without_replacements(&**local_name, encoding)
+                match LocalName::from_str_without_replacements(local_name.into_string(), encoding)
                     .map(LocalName::into_owned)
                 {
-                    Ok(local_name) => self.compile_expr(move |_, actual| *actual == local_name),
+                    Ok(local_name) => {
+                        Self::compile_expr(neg, move |_, actual| *actual == local_name)
+                    }
                     // NOTE: selector value can't be converted to the given encoding, so
                     // it won't ever match.
-                    Err(_) => self.compile_expr(|_, _| false),
+                    Err(_) => Self::compile_expr(neg, |_, _| false),
                 }
             }
-            &OnTagNameExpr::NthChild(nth) => {
-                self.compile_expr(move |state, _| state.cumulative.is_nth(nth))
+            OnTagNameExpr::NthChild(nth) => {
+                Self::compile_expr(neg, move |state, _| state.cumulative.is_nth(nth))
             }
-            &OnTagNameExpr::NthOfType(nth) => {
+            OnTagNameExpr::NthOfType(nth) => {
                 *enable_nth_of_type = true;
-                self.compile_expr(move |state, _| {
+                Self::compile_expr(neg, move |state, _| {
                     state
                         .typed
                         .expect("Counter for type required at this point")
@@ -94,13 +97,11 @@ impl Compilable for Expr<OnTagNameExpr> {
 
 impl Expr<OnAttributesExpr> {
     #[inline]
-    pub fn compile_expr<
-        F: Fn(&SelectorState<'_>, &AttributeMatcher<'_>) -> bool + Send + 'static,
-    >(
-        &self,
+    fn compile_expr<F: Fn(&SelectorState<'_>, &AttributeMatcher<'_>) -> bool + Send + 'static>(
+        negation: bool,
         f: F,
     ) -> CompiledAttributeExpr {
-        if self.negation {
+        if negation {
             Box::new(move |s, a| !f(s, a))
         } else {
             Box::new(f)
@@ -111,24 +112,25 @@ impl Expr<OnAttributesExpr> {
 #[inline]
 fn compile_literal(
     encoding: &'static Encoding,
-    lit: &str,
+    lit: Box<str>,
 ) -> Result<BytesOwned, HasReplacementsError> {
-    Ok(BytesCow::owned_from_str_without_replacements(lit, encoding)?.into())
+    Ok(BytesCow::owned_from_str_without_replacements(lit.into_string(), encoding)?.into())
 }
 
 #[inline]
 fn compile_literal_lowercase(
     encoding: &'static Encoding,
-    lit: &str,
+    mut lit: Box<str>,
 ) -> Result<BytesOwned, HasReplacementsError> {
-    compile_literal(encoding, &lit.to_ascii_lowercase())
+    lit.make_ascii_lowercase();
+    compile_literal(encoding, lit)
 }
 
 #[inline]
 fn compile_operands(
     encoding: &'static Encoding,
-    name: &str,
-    value: &str,
+    name: Box<str>,
+    value: Box<str>,
 ) -> Result<(BytesOwned, BytesOwned), HasReplacementsError> {
     Ok((
         compile_literal_lowercase(encoding, name)?,
@@ -137,54 +139,55 @@ fn compile_operands(
 }
 
 impl Compilable for Expr<OnAttributesExpr> {
-    fn compile(&self, encoding: &'static Encoding, exprs: &mut ExprSet, _: &mut bool) {
-        let expr_result =
-            match &self.simple_expr {
-                OnAttributesExpr::Id(id) => compile_literal(encoding, id)
-                    .map(|id| self.compile_expr(move |_, m| m.has_id(&id))),
+    fn compile(self, encoding: &'static Encoding, exprs: &mut ExprSet, _: &mut bool) {
+        let neg = self.negation;
+        let expr_result = match self.simple_expr {
+            OnAttributesExpr::Id(id) => compile_literal(encoding, id)
+                .map(|id| Self::compile_expr(neg, move |_, m| m.has_id(&id))),
 
-                OnAttributesExpr::Class(class) => compile_literal(encoding, class)
-                    .map(|class| self.compile_expr(move |_, m| m.has_class(&class))),
+            OnAttributesExpr::Class(class) => compile_literal(encoding, class)
+                .map(|class| Self::compile_expr(neg, move |_, m| m.has_class(&class))),
 
-                OnAttributesExpr::AttributeExists(name) => compile_literal(encoding, name)
-                    .map(|name| self.compile_expr(move |_, m| m.has_attribute(&name))),
+            OnAttributesExpr::AttributeExists(name) => compile_literal(encoding, name)
+                .map(|name| Self::compile_expr(neg, move |_, m| m.has_attribute(&name))),
 
-                &OnAttributesExpr::AttributeComparisonExpr(AttributeComparisonExpr {
-                    ref name,
-                    ref value,
+            OnAttributesExpr::AttributeComparisonExpr(AttributeComparisonExpr {
+                name,
+                value,
+                case_sensitivity,
+                operator,
+            }) => compile_operands(encoding, name, value).map(move |(name, value)| {
+                let operands = AttrExprOperands {
+                    name,
+                    value,
                     case_sensitivity,
-                    operator,
-                }) => compile_operands(encoding, name, value).map(move |(name, value)| {
-                    let operands = AttrExprOperands {
-                        name,
-                        value,
-                        case_sensitivity,
-                    };
-                    match operator {
-                        AttrSelectorOperator::Equal => {
-                            self.compile_expr(move |_, m| m.attr_eq(&operands))
-                        }
-                        AttrSelectorOperator::Includes => self
-                            .compile_expr(move |_, m| m.matches_splitted_by_whitespace(&operands)),
-                        AttrSelectorOperator::DashMatch => {
-                            self.compile_expr(move |_, m| m.has_dash_matching_attr(&operands))
-                        }
-                        AttrSelectorOperator::Prefix => {
-                            self.compile_expr(move |_, m| m.has_attr_with_prefix(&operands))
-                        }
-                        AttrSelectorOperator::Suffix => {
-                            self.compile_expr(move |_, m| m.has_attr_with_suffix(&operands))
-                        }
-                        AttrSelectorOperator::Substring => {
-                            self.compile_expr(move |_, m| m.has_attr_with_substring(&operands))
-                        }
+                };
+                match operator {
+                    AttrSelectorOperator::Equal => {
+                        Self::compile_expr(neg, move |_, m| m.attr_eq(&operands))
                     }
-                }),
-            };
+                    AttrSelectorOperator::Includes => Self::compile_expr(neg, move |_, m| {
+                        m.matches_splitted_by_whitespace(&operands)
+                    }),
+                    AttrSelectorOperator::DashMatch => {
+                        Self::compile_expr(neg, move |_, m| m.has_dash_matching_attr(&operands))
+                    }
+                    AttrSelectorOperator::Prefix => {
+                        Self::compile_expr(neg, move |_, m| m.has_attr_with_prefix(&operands))
+                    }
+                    AttrSelectorOperator::Suffix => {
+                        Self::compile_expr(neg, move |_, m| m.has_attr_with_suffix(&operands))
+                    }
+                    AttrSelectorOperator::Substring => {
+                        Self::compile_expr(neg, move |_, m| m.has_attr_with_substring(&operands))
+                    }
+                }
+            }),
+        };
 
         exprs
             .attribute_exprs
-            .push(expr_result.unwrap_or_else(|_| self.compile_expr(|_, _| false)));
+            .push(expr_result.unwrap_or_else(|_| Self::compile_expr(neg, |_, _| false)));
     }
 }
 
@@ -209,7 +212,7 @@ impl Compiler {
         Predicate {
             on_tag_name_exprs,
             on_attr_exprs,
-        }: &Predicate,
+        }: Predicate,
         branch: ExecutionBranch,
         enable_nth_of_type: &mut bool,
     ) -> Instruction {
@@ -281,7 +284,7 @@ impl Compiler {
             };
 
             self.instructions[position] =
-                Some(self.compile_predicate(&node.predicate, branch, enable_nth_of_type));
+                Some(self.compile_predicate(node.predicate, branch, enable_nth_of_type));
         }
 
         addr_range
